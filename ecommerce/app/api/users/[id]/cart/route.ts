e310b6fe7a5f809604089127e07a735d62199008
pravin1707 <pravin1712kumar@gmail.com>
2025-07-24 11:42:56 -0400

    cart page functionalities

--- ecommerce/app/api/users/[id]/cart/route.ts
+++ ecommerce/app/api/users/[id]/cart/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest } from 'next/server';
 import { products } from '@/app/product-data';
+import { pid } from 'process';
 
 type ShoppingCart = Record<string, string[]>;
 
@@ -34,4 +35,40 @@ export async function GET(request: NextRequest, { params }: { params: Params }) 
       'Content-Type': 'application/json'
     }
   });
+}
+
+type CartBody = { 
+  productId: string
+}
+
+export async function POST(request: NextRequest, { params }: {params: Params}) {
+  const userId = params.id;
+  const body: CartBody = await request.json();
+  const productId = body.productId;
+
+  carts[userId] = carts[userId] ? carts[userId].concat(productId) : [productId];
+  const cartProducts = carts[userId].map(id => products.find(p => p.id === id));
+
+  return new Response(JSON.stringify(cartProducts), {
+    status: 201,
+    headers: {
+      'Content-Type': 'application/json',
+    }}
+  );
+}
+
+export async function DELETE(request: NextRequest, { params }: { params: Params }) {
+  const userId = params.id;
+  const body = await request.json();
+  const productId = body.productId;
+
+  carts[userId] = carts[userId] ? carts[userId].filter(pid => pid !== productId) : [];
+  const cartProducts = carts[userId].map(id => products.find(p => p.id === id));
+
+  return new Response(JSON.stringify(cartProducts), {
+    status: 202,
+    headers: {
+      'Content-Type': 'application/json',
+    }}
+  );
 }
