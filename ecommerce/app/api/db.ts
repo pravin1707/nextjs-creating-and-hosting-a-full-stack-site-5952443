711fd48e1ef8520956b9946d8451b9115ff0a816
pravin1707 <pravin1712kumar@gmail.com>
2025-07-25 22:57:28 -0400

    rebuild mongo db

--- ecommerce/app/api/db.ts
+++ ecommerce/app/api/db.ts
@@ -3,12 +3,15 @@ import { MongoClient, Db, ServerApiVersion } from 'mongodb';
 let cachedClient: MongoClient | null = null;
 let cachedDb: Db | null = null;
 
+const user = encodeURIComponent(process.env.MONGO_USER || "");
+const pass = encodeURIComponent(process.env.MONGO_PASSWORD || "");
+
 export async function connectToDb() {
   if (cachedClient && cachedDb) {
     return { client: cachedClient, db: cachedDb };
   }
 
-  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.vqdwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  const uri = `mongodb+srv://${user}:${pass}cluster0.vqdwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
   const client = new MongoClient(uri, {
     serverApi: {
